c51080e24318ceacacee69f5fab3781b97ad2c83
Jesiel Schwengber <jesielelias@outlook.com>
2022-04-03 11:39:01 -0300

    Commit inicial

---
+++ GeekShopping.ProductAPI/Migrations/20220403142448_SeedProductDataTable.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeekShopping.ProductAPI.Migrations
+{
+    public partial class SeedProductDataTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[] { 1L, "Category", "Description", "https://github.com/leandrocgsi/erudio-microservices-dotnet6/blob/main/ShoppingImages/10_milennium_falcon.jpg?raw=true", "Millenium Falcon", 69.9m });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 1L);
+        }
+    }
+}
---
+++ GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext() { }
+
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 1,
+                Name = "Millenium Falcon",
+                Price = new decimal(69.9),
+                Description = "Description",
+                CategoryName = "Category",
+                ImageUrl = "https://github.com/leandrocgsi/erudio-microservices-dotnet6/blob/main/ShoppingImages/10_milennium_falcon.jpg?raw=true"
+            });
+        }
+
+    }
+}
